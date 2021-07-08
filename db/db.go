66342e8635d3aa59209f79d20bb2d1ee83acf52f
aincicau <alexandru.incicau@gmail.com>
2021-07-08 11:37:43 +0300

    conectare db

--- db/db.go
+++ db/db.go
@@ -1,10 +1,25 @@
 package db
 
 import (
+	"fmt"
+
 	_ "github.com/golang-migrate/migrate/database/postgres"
 	"github.com/jinzhu/gorm"
 )
 
+const dbErrorMessage = "Error connecting to Database"
+
+var db *gorm.DB
+
 func InitDatabase(databaseURL string) {
-	gorm.Open("postgres", databaseURL)
+	db, err := gorm.Open("postgres", databaseURL)
+	if err != nil {
+		fmt.Println(dbErrorMessage)
+	}
+
+	fmt.Println(db)
+}
+
+func GetDB() *gorm.DB {
+	return db
 }
--- main.go
+++ main.go
@@ -1,1 +1,7 @@
 package main
+
+import "golangorm/db"
+
+func main() {
+	db.InitDatabase("postgres://postgres:mysecretpassword@localhost:5432/postgres?sslmode=disable")
+}
